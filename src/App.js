f2fe92179f9a64aa0a1e9c8d82e3102ab8e4ee78
uday-sharma <69636211+n-uday-sharma@users.noreply.github.com>
2024-02-07 16:54:11 -0500

    Initial commit
    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import VideoJS from './VideoJS';
+import videojs from 'video.js';
+const App = () => {
+  
+  const playerRef = React.useRef(null);
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [ {
+      src: `https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8`, //add video url here
+      type: "application/x-mpegurl",
+      label: "original",
+      // selected:
+      //   navigator && navigator.connection.effectiveType === "4g" && true,
+    }]
+  };
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+    // You can handle player events here, for example:
+    player.on('waiting', () => {
+     // videojs.log('player is waiting');
+    });
 
-function App() {
+    player.on('dispose', () => {
+      // videojs.log('player will dispose');
+    });
+    console.log(videojs.Vhs.xhr.onRequest);
+    const globalRequestHook = (options) => {
+      console.log(options);
+      options.beforeSend = (xhr) => {
+        xhr.setRequestHeader('foo', 'bar');
+      };
+      return options;
+    };
+    videojs.Vhs.xhr.onRequest(globalRequestHook);
+    console.log(videojs.Vhs.xhr.onRequest);
+    player.on('xhr-hooks-ready', () => {
+      console.log('test');
+      const playerXhrRequestHook = (options) => {
+        options.beforeSend = (xhr) => {
+          xhr.setRequestHeader('foo', 'bar');
+        };
+        return options;
+      };
+      player.tech().vhs.xhr.onRequest(playerXhrRequestHook);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Rest of app here</div>
+      <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+      <div>Rest of app here</div>
+    </>
   );
 }
 
